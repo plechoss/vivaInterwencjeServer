1e311b3c2ac81763340b66ab097d59fac14ac70f
unknown <plaskacz.panda@gmail.com>
2021-12-23 14:22:19 +0100

    Testing

--- server.py
+++ server.py
@@ -12,9 +12,9 @@ allegro_timestamp = datetime.now()
 @app.route('/allegro', methods=['GET'])
 def home():
     timestamp = datetime.now()
-    if((allegro_data is None) or (timestamp - allegro_timestamp).seconds / 60):
-        allegro_data = requests.get('https://w3schools.com/python/demopage.htm')
-        print(allegro_data)
+    #if((allegro_data is None) or (timestamp - allegro_timestamp).seconds / 60 > 5):
+    allegro_data = requests.get('https://w3schools.com/python/demopage.htm')
+    print(allegro_data)
     return allegro_data, 200
 
 
